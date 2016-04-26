5d88d34ad01328d63022787f1fd344f9adc99979
MarioPecher <mario.pecher.mpr@googlemail.com>
2016-04-26 10:52:54 +0200

    Added files via upload
    upload of the initial Files

---
+++ DesktopStartup.bat
@@ -0,0 +1,28 @@
+:: Desktop Statup Batch
+:: Version 0.1
+
+:: erstellt durch Mario Pecher
+:: erstellt am 26.04.2016
+
+:: geändert durch Mario Pecher
+:: geändert am 26.04.2016
+
+:: Version History
+::------------------------
+:: Version 0.2 ???
+::------------------------
+:: Version 0.1
+::	add Taskmanager
+::	add Outlook
+::	add Firefox
+::
+::------------------------
+
+:: Taskmanager start
+::"C:\Windows\System32\taskmgr.exe"
+
+:: Outlook start
+::"C:\Program Files (x86)\Microsoft Office\Office12\OUTLOOK.exe"
+
+:: Firefox start
+"C:\Programm Files (x86)\Mozilla Firefox\firefox.exe"
